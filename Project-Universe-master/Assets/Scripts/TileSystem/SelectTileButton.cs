ead8eb6e9c8c9f33c79eb882c42255fd4e757fb8
Peter <skynet7500@gmail.com>
2019-04-10 21:10:19 +0200

    Reading XML, runtime Database build, UI runtime build, Assembling tiles from database

---
+++ Project-Universe-master/Assets/Scripts/TileSystem/SelectTileButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectTileButton : MonoBehaviour
+{
+
+    public int TileID;
+    public GameObject Cube;
+    private GameObject tile;
+    private GameObject duplicate;
+    private string path;
+
+    public void Start()
+    {
+        Cube = GameObject.Find("TileGhost");
+        Debug.Log(Cube);
+    }
+
+    public void Update()
+    {
+
+    }
+
+
+    public void WhenClicked(int a)
+    {
+
+        foreach (Transform child in Cube.transform)
+            Destroy(child.gameObject);
+
+
+
+
+        path = "Tile_Database/" + TileID.ToString();
+        tile = GameObject.Find(path);
+        //Cube = tile;
+        duplicate = Instantiate<GameObject>(tile);
+        // tile.SetActive(true);
+
+        duplicate.transform.SetParent(Cube.transform);
+        duplicate.transform.localPosition = new Vector3(0, 0, 0);
+
+        foreach (Transform child in duplicate.transform)
+            child.gameObject.layer = 0;
+    }
+
+}
--- Project-Universe-master/Assets/Scripts/TileSystem/TilingController.cs
+++ Project-Universe-master/Assets/Scripts/TileSystem/TilingController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.UI;
 
 
 
@@ -10,6 +11,7 @@ public class TilingController : MonoBehaviour
 
     public Camera Cam;
     public GameObject Tile;
+    private MeshFilter mesh;
     public bool AllowBuilding;
     private bool SnapToGrid;
 
@@ -18,23 +20,39 @@ public class TilingController : MonoBehaviour
 
     private bool sw = false;
 
+    //  public BuildMenu buildMenu;
 
+    public GameObject TileContainer;
+    public GameObject BuildingMenu;
+    public GameObject Button;
+    private GameObject temp;
+    private string FilePath;
+
+    private GameObject newButton;
+    private SelectTileButton but;
+
+    //  public TileCollection tileContainer = TileCollection.Load(Path.Combine(Application.dataPath, "Tiles.xml"));
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        // buildMenu = GameObject.Find("EventSystem").GetComponent<BuildMenu>();
         SnapToGrid = true;
 
         ReadXMLTiles();
 
-      /* SAVING XML FILE FOR TESTING - THIS WILL GO FOR EXTERIOR EDITOR
-        if (sw == false)
-       {
-           SaveXMLTiles();
-           sw = true;
-        }
-      */
+        /* SAVING XML FILE FOR TESTING - THIS WILL GO FOR EXTERIOR EDITOR
+          if (sw == false)
+         {
+             SaveXMLTiles();
+             sw = true;
+          }
+        */
+
+        // Mesh model = (Mesh)Resources.Load("Models/Tiles/Floors/nukeguard/mesh1", typeof(Mesh));
+        // Mesh model = Resources.LoadAll<Mesh>("Models/Tiles/Floors/nukeguard/mesh1")[5];
+
+
     }
 
 
@@ -42,7 +60,7 @@ public class TilingController : MonoBehaviour
     void Update()
     {
 
-      
+
 
         if (AllowBuilding)
         {
@@ -58,7 +76,7 @@ public class TilingController : MonoBehaviour
                 {
                     //this is for debug on Scene, draw line for raycasting
                     Debug.DrawLine(hit.point, debugrayend, Color.red);
-                    Debug.Log(hit.point);
+                 //   Debug.Log(hit.point);
 
                     if (hit.transform.gameObject.tag != "TilingGhost")  // prevents from raycasting tile
 
@@ -84,17 +102,62 @@ public class TilingController : MonoBehaviour
     public void ReadXMLTiles()
     {
         var tileContainer = TileCollection.Load(Path.Combine(Application.dataPath, "Tiles.xml"));
-       // var xmlData = @"<TileCollection><tiles><Tiles name=""a""><model_path></model_path>x<material_path>y</material_path></Tiles></tiles></TileCollection>";
-       // var tileContainer = TileCollection.LoadFromText(xmlData);
-        Debug.Log("Number of tiles in database: " + tileContainer.tiles.Length);
-        Debug.Log(tileContainer.tiles[0].model_path);
+             // var xmlData = @"<TileCollection><tiles><Tiles name=""a""><model_path></model_path>x<material_path>y</material_path></Tiles></tiles></TileCollection>";
+             // var tileContainer = TileCollection.LoadFromText(xmlData);
+       // Debug.Log("Number of tiles in database: " + tileContainer.tiles.Length);
+      //  Debug.Log(tileContainer.tiles[0].model_path);
+
+       // AddButton(tileContainer.tiles.Length);  //ADD BUTTONS TO UI
+
+
+        var parentDatabase = new GameObject();
+        parentDatabase.name = "Tile_Database";
+        parentDatabase.layer = 10;
+        for (int c = 0; c < tileContainer.tiles.Length; c++)
+        {
+            FilePath = tileContainer.tiles[c].model_path;
+            Debug.Log(tileContainer.tiles[c].model_path);
+            //  FilePath = "Models/Tiles/Floors/nukeguard/mesh1";
+            GameObject model = Resources.Load<GameObject>(FilePath);
+            GameObject obj = (GameObject)Instantiate(model);
+            obj.transform.Find("Camera").GetComponent<Camera>().enabled = false;
+
+            foreach (Transform child in obj.transform)
+                child.gameObject.layer = 10;
+
+                // obj.SetActive(false);
+                obj.transform.SetParent(parentDatabase.transform);
+            // obj.name = tileContainer.tiles[c].Name;
+            obj.name = c.ToString() ;
+
+
+            newButton = Instantiate(Button);
+            but = newButton.GetComponent<SelectTileButton>();
+            but.TileID = c;
+            newButton.GetComponentInChildren<Text>().text = tileContainer.tiles[c].Name;
+            newButton.transform.SetParent(BuildingMenu.transform);
+          
+        }
 
     }
 
     public void SaveXMLTiles()
     {
         var tileContainer = TileCollection.Load(Path.Combine(Application.dataPath, "Tiles.xml"));
-          tileContainer.Save(Path.Combine(Application.persistentDataPath, "Tiles.xml"));
+        tileContainer.Save(Path.Combine(Application.persistentDataPath, "Tiles.xml"));
+    }
+
+    //---------------------BUILDING UI--------------------------
+
+    public void AddButton(int i)
+    {
+
+        for (int c = 0; c < i; c++)
+        {
+            var newButton = Instantiate(Button);
+            newButton.transform.SetParent(BuildingMenu.transform);
+        }
+
     }
 }
 
