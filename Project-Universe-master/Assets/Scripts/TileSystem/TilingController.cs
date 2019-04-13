fe68c87550c698b46b982d4b34e8fd3d9e7566ab
Peter <skynet7500@gmail.com>
2019-04-13 13:34:53 +0200

    LOD runtime assembly, tile hierarhy in FBX test

--- Project-Universe-master/Assets/Scripts/TileSystem/TilingController.cs
+++ Project-Universe-master/Assets/Scripts/TileSystem/TilingController.cs
@@ -76,7 +76,7 @@ public class TilingController : MonoBehaviour
                 {
                     //this is for debug on Scene, draw line for raycasting
                     Debug.DrawLine(hit.point, debugrayend, Color.red);
-                 //   Debug.Log(hit.point);
+                    //   Debug.Log(hit.point);
 
                     if (hit.transform.gameObject.tag != "TilingGhost")  // prevents from raycasting tile
 
@@ -97,17 +97,19 @@ public class TilingController : MonoBehaviour
     }
 
 
+    public LODGroup group;
+
     //READING / SAVING XML FILE
 
     public void ReadXMLTiles()
     {
         var tileContainer = TileCollection.Load(Path.Combine(Application.dataPath, "Tiles.xml"));
-             // var xmlData = @"<TileCollection><tiles><Tiles name=""a""><model_path></model_path>x<material_path>y</material_path></Tiles></tiles></TileCollection>";
-             // var tileContainer = TileCollection.LoadFromText(xmlData);
-       // Debug.Log("Number of tiles in database: " + tileContainer.tiles.Length);
-      //  Debug.Log(tileContainer.tiles[0].model_path);
+        // var xmlData = @"<TileCollection><tiles><Tiles name=""a""><model_path></model_path>x<material_path>y</material_path></Tiles></tiles></TileCollection>";
+        // var tileContainer = TileCollection.LoadFromText(xmlData);
+        // Debug.Log("Number of tiles in database: " + tileContainer.tiles.Length);
+        //  Debug.Log(tileContainer.tiles[0].model_path);
 
-       // AddButton(tileContainer.tiles.Length);  //ADD BUTTONS TO UI
+        // AddButton(tileContainer.tiles.Length);  //ADD BUTTONS TO UI
 
 
         var parentDatabase = new GameObject();
@@ -120,15 +122,17 @@ public class TilingController : MonoBehaviour
             //  FilePath = "Models/Tiles/Floors/nukeguard/mesh1";
             GameObject model = Resources.Load<GameObject>(FilePath);
             GameObject obj = (GameObject)Instantiate(model);
-            obj.transform.Find("Camera").GetComponent<Camera>().enabled = false;
+
+            //  obj.AddComponent(typeof(LODGroup));
+
 
             foreach (Transform child in obj.transform)
                 child.gameObject.layer = 10;
 
-                // obj.SetActive(false);
-                obj.transform.SetParent(parentDatabase.transform);
+            // obj.SetActive(false);
+            obj.transform.SetParent(parentDatabase.transform);
             // obj.name = tileContainer.tiles[c].Name;
-            obj.name = c.ToString() ;
+            obj.name = c.ToString();
 
 
             newButton = Instantiate(Button);
@@ -136,9 +140,49 @@ public class TilingController : MonoBehaviour
             but.TileID = c;
             newButton.GetComponentInChildren<Text>().text = tileContainer.tiles[c].Name;
             newButton.transform.SetParent(BuildingMenu.transform);
-          
-        }
 
+            // TO REMOVE WHEN ALL TILES IS UNIFIED
+            if (obj.transform.Find("Camera") != null)
+            {
+                obj.transform.Find("Camera").GetComponent<Camera>().enabled = false;
+            }
+            // END REMOVE
+
+
+            //obj.GetComponent<LODGroup>().
+            // /*
+            if (obj.transform.Find("model") != null)
+            {
+
+                group = obj.AddComponent<LODGroup>();
+
+                // Add 4 LOD levels
+                LOD[] lods = new LOD[4];
+
+                for (int i = 0; i < 4; i++)
+                {
+                    GameObject primType = obj.transform.Find("model").gameObject;
+                    switch (i)
+                    {
+                        case 1:
+                            primType = obj.transform.Find("lod1").gameObject;
+                            break;
+                        case 2:
+                            primType = obj.transform.Find("lod2").gameObject;
+                            break;
+                        case 3:
+                            primType = obj.transform.Find("lod3").gameObject;
+                            break;
+                    }
+
+                    Renderer[] renderers = new Renderer[1];
+                    renderers[0] = primType.GetComponentInChildren<Renderer>();
+                    lods[i] = new LOD(1.0F / (i + 1.2f), renderers);
+                }
+                group.SetLODs(lods);
+                group.RecalculateBounds();
+            }
+        }
     }
 
     public void SaveXMLTiles()
