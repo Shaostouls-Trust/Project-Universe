a261fe68defe5e5f4ac2eed83205bc0933f27a29
KhorneSyrup <Psychologicalresiliencemk0@live.com>
2020-02-12 19:59:18 -0800

    Replacing missing files.

---
+++ Project Universe/Assets/Scripts/KhorneSyrup/Player Controller/Interaction/Door.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour, IInteractable
+{
+    private bool closed = true;
+    private bool locked = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void ActDoor() {
+        if (closed)
+        {
+            closed = !closed;
+        }
+        if (locked)
+        {
+            Debug.Log("Door is locked!");
+            return;
+        }
+        else {
+
+            closed = !closed;
+        }
+    }
+
+
+    void LockDoor() {
+        locked = !locked;
+    }
+
+    public void Interact()
+    {
+        ActDoor();
+    }
+
+    public void RecieveDamage(float damage)
+    {
+        throw new System.NotImplementedException();
+    }
+}
