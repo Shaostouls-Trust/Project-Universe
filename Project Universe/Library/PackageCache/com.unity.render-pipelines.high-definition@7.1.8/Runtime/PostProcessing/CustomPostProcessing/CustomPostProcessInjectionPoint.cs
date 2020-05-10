382d462ca6f70766e5043f5e4347aaa235754f6a
Shaostoul <Shaostoul@Yahoo.com>
2020-05-10 02:06:25 -0700

    Removed Library Folder

--- Project Universe/Library/PackageCache/com.unity.render-pipelines.high-definition@7.1.8/Runtime/PostProcessing/CustomPostProcessing/CustomPostProcessInjectionPoint.cs
+++
@@ -1,9 +0,0 @@
-namespace UnityEngine.Rendering.HighDefinition
-{
-    public enum CustomPostProcessInjectionPoint
-    {
-        AfterOpaqueAndSky,
-        BeforePostProcess,
-        AfterPostProcess,
-    }
-}
